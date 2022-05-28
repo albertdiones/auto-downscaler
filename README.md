4cf546e50cdb0ac97208c13173c23b61543f0e50
Albert Diones <albertdiones@gmail.com>
2022-05-28 12:41:20 +0800

    version1 (working and functional)

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
+Note: This requires HandbrakeCLI
 
-A simple automatic downscaler that I can use for Davinci Resolve proxy
+Readmore about HandbrakeCLI here:
+https://handbrake.fr/docs/en/latest/cli/cli-options.html
 
-There should a config yaml that will choose which folders and files are gonna be downscaled
+A simple automatic downscaler that can be used for Davinci Resolve proxy or downscaled backups
+
+Copy config.yml.dist to config.yml and edit it according to your needs
 
 downscale.py:
-   # Should check the files from the source folder
+   # Will scan the files from the source folder
    # Then check if it exists on the target folder
    # If it doesn't exist, downscale it using Handbrake
    # If it exists, skip
-
-
-I expect this will speed up Davinci resolve's shuttle and scroll function
--- downscale.py
+++ downscale.py
@@ -14,26 +14,55 @@ quality = configs['quality']
 
 inputBaseDirectory=configs['input_base_dir']
 
-outputDirectory = configs['ouput_dir']
+outputBaseDirectory = configs['ouput_dir']
+
+handbrakePathname = configs['handbrakecli_pathname'];
+
+eParameter = f"{configs['handbrakecli_extra_parameters']}" if configs.get('handbrakecli_extra_parameters') != None else '';
 
 print(f"Input Dir: {inputBaseDirectory}")
-print(f"Output Dir: {outputDirectory}")
+print(f"Output Dir: {outputBaseDirectory}")
+
+if not os.path.isdir(outputBaseDirectory):
+    os.makedirs(outputBaseDirectory)
 
 encodedCount=0
+
+commands=[]
 
 for directory, directoryConfig in configs['directories'].items():
     print(directory)
     #print(directoryConfig)
+    outpuDirectory =f"{outputBaseDirectory}/{directory}";
+
+    if not os.path.isdir(outpuDirectory):
+        os.makedirs(outpuDirectory);    
 
     for filePattern in directoryConfig['filename_patterns']:
         
         directoryFilePattern=f"{inputBaseDirectory}/{directory}/{filePattern}"
     
         for fileName in glob.glob(directoryFilePattern):
-            print(f"Found file: {fileName}") 
+            #print(f"Found file: {fileName}") 
 
             newBaseName = os.path.basename(fileName)
 
-            newFileName=f"{outputDirectory}/{directory}/{newBaseName}"
-            print(f"Encoding to {newFileName}")
-            sleep(0.5);
+            newFileName=f"{outpuDirectory}/{newBaseName}"
+            #print(f"Encoding to {newFileName}")
+
+            if os.path.isfile(newFileName):
+                continue
+
+            command = f"{handbrakePathname} -i \"{fileName}\" -o \"{newFileName}\" -q{str(quality)} {eParameter}"
+
+            commands.append(command)
+
+
+for command in commands:
+    print(command)
+
+if input("Do you want to run these commands? (yes/n)")[0].lower() !='yes':
+    print("You have cancelled the commands")
+
+for command in commands:
+    os.system(command)
